61daa370a0040fe969f9262503b56391858490ab
Milan Koladiya <milankoladiya444@gmail.com>
2020-10-10 19:20:13 +0530

    to fix some issue

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -6,5 +7,11 @@ const PORT = process.env.PORT || 8080;
 app.get("/ping", (req, res) => {
   res.send("Milan-Koladiya");
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "front-end", "index.html"));
+  });
+}
 
 app.listen(PORT);
